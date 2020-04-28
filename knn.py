831126e47ebf31983aea6910bb2baf8390edea8b
aleksalukac <aleksa@lukac.rs>
2020-04-29 01:50:06 +0200

    Update knn.py

--- knn.py
+++ knn.py
@@ -82,14 +82,4 @@ for cluster in clusters:
         
     i += 1
         
-
-"""
-point1 = [1, 2]
-point2 = [3, 4]
-
-x_values = [point1[0], point2[0]]
-
-y_values = [point1[1], point2[1]]
-
-plt.plot(x_values, y_values)"""
 plt.show()
